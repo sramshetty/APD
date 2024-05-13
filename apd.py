44d0a557be2d87a00d8e725bd2470eae0c03a6bf
sramshetty <shivaen.ramshetty@gmail.com>
2024-05-12 21:45:15 -0700

    avoid building vision encoder

--- apd.py
+++ apd.py
@@ -8,113 +8,40 @@ import torch.nn.functional as F
 
 from llm2vec import LLM2Vec
 
-from open_clip import CLIPVisionCfg
-from open_clip.modified_resnet import ModifiedResNet
-from open_clip.timm_model import TimmModel
-from open_clip.transformer import (
-    LayerNormFp32,
-    LayerNorm,
-    QuickGELU,
-    VisionTransformer
-)
-
-
-
-def _build_vision_tower(
-        embed_dim: int,
-        vision_cfg: CLIPVisionCfg,
-        quick_gelu: bool = False,
-        cast_dtype: Optional[torch.dtype] = None
-):
-    if isinstance(vision_cfg, dict):
-        vision_cfg = CLIPVisionCfg(**vision_cfg)
-
-    # OpenAI models are pretrained w/ QuickGELU but native nn.GELU is both faster and more
-    # memory efficient in recent PyTorch releases (>= 1.10).
-    # NOTE: timm models always use native GELU regardless of quick_gelu flag.
-    act_layer = QuickGELU if quick_gelu else nn.GELU
-
-    if vision_cfg.timm_model_name:
-        visual = TimmModel(
-            vision_cfg.timm_model_name,
-            pretrained=vision_cfg.timm_model_pretrained,
-            pool=vision_cfg.timm_pool,
-            proj=vision_cfg.timm_proj,
-            proj_bias=vision_cfg.timm_proj_bias,
-            drop=vision_cfg.timm_drop,
-            drop_path=vision_cfg.timm_drop_path,
-            patch_drop=vision_cfg.patch_dropout if vision_cfg.patch_dropout > 0 else None,
-            embed_dim=embed_dim,
-            image_size=vision_cfg.image_size,
-        )
-    elif isinstance(vision_cfg.layers, (tuple, list)):
-        vision_heads = vision_cfg.width * 32 // vision_cfg.head_width
-        visual = ModifiedResNet(
-            layers=vision_cfg.layers,
-            output_dim=embed_dim,
-            heads=vision_heads,
-            image_size=vision_cfg.image_size,
-            width=vision_cfg.width,
-        )
-    else:
-        vision_heads = vision_cfg.width // vision_cfg.head_width
-        norm_layer = LayerNormFp32 if cast_dtype in (torch.float16, torch.bfloat16) else LayerNorm
-        if vision_cfg.norm_kwargs:
-            norm_layer = partial(norm_layer, **vision_cfg.norm_kwargs)
-        if vision_cfg.act_kwargs is not None:
-            act_layer = partial(act_layer, **vision_cfg.act_kwargs)
-
-        visual = VisionTransformer(
-            image_size=vision_cfg.image_size,
-            patch_size=vision_cfg.patch_size,
-            width=vision_cfg.width,
-            layers=vision_cfg.layers,
-            heads=vision_heads,
-            mlp_ratio=vision_cfg.mlp_ratio,
-            ls_init_value=vision_cfg.ls_init_value,
-            patch_dropout=vision_cfg.patch_dropout,
-            attentional_pool=vision_cfg.attentional_pool,
-            attn_pooler_queries=vision_cfg.attn_pooler_queries,
-            attn_pooler_heads=vision_cfg.attn_pooler_heads,
-            pos_embed_type=vision_cfg.pos_embed_type,
-            no_ln_pre=vision_cfg.no_ln_pre,
-            final_ln_after_pool=vision_cfg.final_ln_after_pool,
-            pool_type=vision_cfg.pool_type,
-            output_tokens=vision_cfg.output_tokens,
-            output_dim=embed_dim,
-            act_layer=act_layer,
-            norm_layer=norm_layer,
-        )
-
-    return visual
-
 
 class APDModel(nn.Module):
     output_dict: torch.jit.Final[bool]
 
     def __init__(
-            self,
-            embed_dim: int,
-            vision_cfg: CLIPVisionCfg,
-            text_enc: LLM2Vec,
-            text_proj_sizes: List[int],
-            num_text_proj_layer: int = 4,
-            quick_gelu: bool = False,
-            init_logit_scale: float = np.log(1 / 0.07),
-            init_logit_bias: Optional[float] = None,
-            cast_dtype: Optional[torch.dtype] = None,
-            output_dict: bool = False,
+        self,
+        vision_enc: nn.Module,
+        text_enc: LLM2Vec,
+        text_enc_output_dim: int,
+        text_proj_sizes: List[int],
+        init_logit_scale: float = np.log(1 / 0.07),
+        init_logit_bias: Optional[float] = None,
+        output_dict: bool = False,
     ):
         super().__init__()
         self.output_dict = output_dict
 
-        self.visual = _build_vision_tower(embed_dim, vision_cfg, quick_gelu, cast_dtype)
+        self.vision_enc = vision_enc
+        self.lock_image_tower()
 
         self.text_enc = text_enc
-        self.text_proj = nn.ModuleList([nn.Linear(_, text_proj_sizes[0], bias=False)])
-        assert len(text_proj_sizes) == num_text_proj_layer, "Need to provide 'num_text_proj_layer' hidden sizes"
-        for k in text_proj_sizes[1:]:
-            self.text_proj.append(nn.SiLU())
+        # freeze text tower
+        for param in self.text_enc.parameters():
+            param.requires_grad = False
+
+        # create projector for pretrained text encoder to match output dim and
+        assert len(text_proj_sizes) > 0 
+        assert self.vision_enc.output_dim == text_proj_sizes[-1], \
+            "Output dim of text projector != Output dim of vision encoder"
+        self.text_proj = nn.ModuleList(
+            [nn.Linear(text_enc_output_dim, text_proj_sizes[0], bias=False)]
+        )
+        for k in range(1, len(text_proj_sizes)):
+            self.text_proj.append(nn.ReLU())
             self.text_proj.append(nn.Linear(text_proj_sizes[k-1], text_proj_sizes[k], bias=False))
 
         self.logit_scale = nn.Parameter(torch.ones([]) * init_logit_scale)
@@ -125,19 +52,19 @@ class APDModel(nn.Module):
 
     def lock_image_tower(self, unlocked_groups=0, freeze_bn_stats=False):
         # lock image tower as per LiT - https://arxiv.org/abs/2111.07991
-        self.visual.lock(unlocked_groups=unlocked_groups, freeze_bn_stats=freeze_bn_stats)
+        self.vision_enc.lock(unlocked_groups=unlocked_groups, freeze_bn_stats=freeze_bn_stats)
 
     @torch.jit.ignore
     def set_grad_checkpointing(self, enable=True):
-        self.visual.set_grad_checkpointing(enable)
+        self.vision_enc.set_grad_checkpointing(enable)
         self.transformer.grad_checkpointing = enable
 
     def encode_image(self, image, normalize: bool = False):
-        features = self.visual(image)
+        features = self.vision_enc(image)
         return F.normalize(features, dim=-1) if normalize else features
 
     def encode_text(self, text, normalize: bool = False):
-        x = self.text_enc.encode(text, convert_to_tensor=True)
+        x = self.text_enc.encode(text, show_progress_bar=False)
         for layer in self.text_proj:
             x = layer(x)
         return F.normalize(x, dim=-1) if normalize else x
@@ -152,9 +79,9 @@ class APDModel(nn.Module):
         return image_logits, text_logits
 
     def forward(
-            self,
-            image: Optional[torch.Tensor] = None,
-            text: Optional[torch.Tensor] = None,
+        self,
+        image: Optional[torch.Tensor] = None,
+        text: Optional[torch.Tensor] = None,
     ):
         image_features = self.encode_image(image, normalize=True) if image is not None else None
         text_features = self.encode_text(text, normalize=True) if text is not None else None
